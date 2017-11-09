a60fd51d765308d2ec83a7c04bed61107c31fb5f
Leslie Hsu <chuckleberries@hotmail.com>
2017-11-09 16:56:19 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,14 @@
 def speak_to_grandma(statement)
-  if statement == "I LOVE YOU GRANDMA"
+  if statement == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif statement.upcase == statement
     "NO, NOT SINCE 1938!"
   else
-    "HUH?! SPEAK UP SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
+  end
 end
+
+
 
 # Write a speak_to_grandma method.
 
